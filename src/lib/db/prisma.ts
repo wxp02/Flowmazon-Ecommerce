d17ecda093af6ba091d751b457a71f664e6cf76c
wxp02 <wxp0025@gmail.com>
2023-10-31 00:06:23 -0400

    Add product page

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
