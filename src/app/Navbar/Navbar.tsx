b9acca2b1860a14808e8d791ecec1b940a6a8a07
wxp02 <wxp0025@gmail.com>
2023-11-22 22:56:08 -0500

    Add user login

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,45 +1,50 @@
-import Image from "next/image"
-import Link from "next/link";
 import logo from "@/assets/logo.png";
-import { redirect } from "next/navigation"
 import { getCart } from "@/lib/db/cart";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 import ShoppingCartButton from "./ShoppingCartButton";
+import UserMenuButton from "./UserMenuButton";
 
 async function searchProducts(formData: FormData) {
-    "use server"
-    
-    const searchQuery = formData.get("searchQuery")?.toString();
+  "use server";
 
-    if (searchQuery) {
-        redirect("/search?query=" + searchQuery);
-    }
+  const searchQuery = formData.get("searchQuery")?.toString();
+
+  if (searchQuery) {
+    redirect("/search?query=" + searchQuery);
+  }
 }
 
 export default async function Navbar() {
-    const cart = await getCart();
+  const session = await getServerSession(authOptions);
+  const cart = await getCart();
 
-    return (
-        <div className="bg-base-100">
-            <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
-                <div className="flex-1">
-                    <Link href="/" className="btn btn-ghost text-xl normal-case">
-                        <Image src={logo} height={40} width={40} alt="Flowmazon logo" />
-                        Flowmazon
-                    </Link>
-                </div>
-                <div className="flex-none gap-2">
-                    <form action={searchProducts}>
-                        <div className="form-control">
-                            <input
-                                name="searchQuery"
-                                placeholder="Search"
-                                className="input input-bordered w-full min-w-[100px]"
-                            />
-                        </div>
-                    </form>
-                    <ShoppingCartButton cart={cart} />
-                </div>
-            </div>
+  return (
+    <div className="bg-base-100">
+      <div className="navbar m-auto max-w-7xl flex-col gap-2 sm:flex-row">
+        <div className="flex-1">
+          <Link href="/" className="btn-ghost btn text-xl normal-case">
+            <Image src={logo} height={40} width={40} alt="Flowmazon logo" />
+            Flowmazon
+          </Link>
         </div>
-    )
+        <div className="flex-none gap-2">
+          <form action={searchProducts}>
+            <div className="form-control">
+              <input
+                name="searchQuery"
+                placeholder="Search"
+                className="input-bordered input w-full min-w-[100px]"
+              />
+            </div>
+          </form>
+          <ShoppingCartButton cart={cart} />
+          <UserMenuButton session={session} />
+        </div>
+      </div>
+    </div>
+  );
 }
