e50feff82d2ae989a7453416d608b2223e16095e
wxp02 <wxp0025@gmail.com>
2023-11-22 23:28:18 -0500

    Merge user cart with anonymous cart

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,7 @@ import { Adapter } from "next-auth/adapters";
 import { env } from "@/lib/env";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma) as Adapter,
@@ -13,8 +14,19 @@ export const authOptions: NextAuthOptions = {
             clientId: env.GOOGLE_CLIENT_ID,
             clientSecret: env.GOOGLE_CLIENT_SECRET
         })
-    ]
-}
+    ],
+    callbacks: {
+        session({ session, user }) {
+            session.user.id = user.id
+            return session;
+        },
+    },
+    events: {
+        async signIn({user}) {
+            await mergeAnonymousCartIntoUserCart(user.id);
+        },
+    },
+};
 
 const handler = NextAuth(authOptions)
 
